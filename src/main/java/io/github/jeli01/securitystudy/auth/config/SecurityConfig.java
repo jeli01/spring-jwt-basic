918883d1a6a42a22d9dd347742ce93bd892f48bc
jeli01 <jeli01@naver.com>
2025-02-22 23:38:34 +0900

    cors 설정 완료

--- src/main/java/io/github/jeli01/securitystudy/auth/config/SecurityConfig.java
+++ src/main/java/io/github/jeli01/securitystudy/auth/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package io.github.jeli01.securitystudy.auth.config;
 import io.github.jeli01.securitystudy.auth.filter.JWTFilter;
 import io.github.jeli01.securitystudy.auth.filter.LoginFilter;
 import io.github.jeli01.securitystudy.auth.jwt.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +15,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -66,6 +70,25 @@ public class SecurityConfig {
         //세션 설정
         http.sessionManagement((session) -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                })));
 
         return http.build();
     }
